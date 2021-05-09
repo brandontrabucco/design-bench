a2b368db6a2fef17c039fec0cbbabeeb91edb705
Brandon Trabucco <brandon@btrabucco.com>
2021-05-09 04:50:42 -0700

    fix return type

--- design_bench/oracles/tensorflow/transformer_oracle.py
+++ design_bench/oracles/tensorflow/transformer_oracle.py
@@ -325,4 +325,4 @@ class TransformerOracle(TensorflowOracle):
         """
 
         # call the model's predict function to generate predictions
-        return self.model["model"].predict(x)
+        return self.model["model"].predict(x).numpy().astype(np.float32)
