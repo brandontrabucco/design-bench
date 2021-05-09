e9c8078edcca538841ac23050c7436741dcb9663
Brandon Trabucco <brandon@btrabucco.com>
2021-05-09 04:17:16 -0700

    update tasks with exact oracles

--- design_bench/__init__.py
+++ design_bench/__init__.py
@@ -13,3 +13,39 @@ register('GFP-GP-v0',
                             max_percentile=100,
                             min_percentile=50,
                             kernel=ProteinKernel()))
+
+
+register('TFBind8-Exact-v0',
+         'design_bench.datasets.discrete:TFBind8Dataset',
+         'design_bench.oracles.exact:TFBind8Oracle',
+         dataset_kwargs=dict(max_samples=5000,
+                             max_percentile=60,
+                             min_percentile=50),
+         oracle_kwargs=dict(noise_std=0.0))
+
+
+register('TFBind10-Exact-v0',
+         'design_bench.datasets.discrete:TFBind10Dataset',
+         'design_bench.oracles.exact:TFBind10Oracle',
+         dataset_kwargs=dict(max_samples=5000,
+                             max_percentile=60,
+                             min_percentile=50),
+         oracle_kwargs=dict(noise_std=0.0))
+
+
+register('NASBench-Exact-v0',
+         'design_bench.datasets.discrete:NASBenchDataset',
+         'design_bench.oracles.exact:NASBenchOracle',
+         dataset_kwargs=dict(max_samples=5000,
+                             max_percentile=60,
+                             min_percentile=50),
+         oracle_kwargs=dict(noise_std=0.0))
+
+
+register('HopperController-Exact-v0',
+         'design_bench.datasets.continuous:HopperControllerDataset',
+         'design_bench.oracles.exact:HopperControllerOracle',
+         dataset_kwargs=dict(max_samples=5000,
+                             max_percentile=60,
+                             min_percentile=50),
+         oracle_kwargs=dict(noise_std=0.0))
---
+++ design_bench/oracles/exact/__init__.py
@@ -0,0 +1,4 @@
+from .hopper_controller_oracle import HopperControllerOracle
+from .nas_bench_oracle import NASBenchOracle
+from .tf_bind_8_oracle import TFBind8Oracle
+from .tf_bind_10_oracle import TFBind10Oracle
