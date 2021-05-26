e0e9b9ce39cb2a4065dc172794ddf6773513073f
Brandon Trabucco <brandon@btrabucco.com>
2021-05-25 23:29:05 -0700

    update version

--- build_all.py
+++
@@ -1,6 +0,0 @@
-import design_bench as db
-
-db.make("GFP-ResNet-v0", oracle_kwargs=dict(fit=True))
-db.make("TFBind8-ResNet-v0", oracle_kwargs=dict(fit=True))
-db.make("ChEMBL-ResNet-v0", oracle_kwargs=dict(fit=True))
-db.make("UTR-ResNet-v0", oracle_kwargs=dict(fit=True))
