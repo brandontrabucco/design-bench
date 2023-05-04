cd43f296f2a7f4447114cd59c95802a20a6844a6
Federico Berto <berto.federico2@gmail.com>
2023-05-04 22:43:48 +0900

    Avoid bug when importing oracles without Mujoco

--- design_bench/oracles/exact/__init__.py
+++ design_bench/oracles/exact/__init__.py
@@ -1,8 +1,39 @@
-from .hopper_controller_oracle import HopperControllerOracle
-from .ant_morphology_oracle import AntMorphologyOracle
-from .dkitty_morphology_oracle import DKittyMorphologyOracle
-from .toy_continuous_oracle import ToyContinuousOracle
-from .nas_bench_oracle import NASBenchOracle
-from .tf_bind_8_oracle import TFBind8Oracle
-from .tf_bind_10_oracle import TFBind10Oracle
-from .toy_discrete_oracle import ToyDiscreteOracle
+try:
+    from .hopper_controller_oracle import HopperControllerOracle
+except ImportError:
+    pass
+
+try:
+    from .ant_morphology_oracle import AntMorphologyOracle
+except ImportError:
+    pass
+
+try:
+    from .dkitty_morphology_oracle import DKittyMorphologyOracle
+except ImportError:
+    pass
+
+try:
+    from .toy_continuous_oracle import ToyContinuousOracle
+except ImportError:
+    pass
+
+try:
+    from .nas_bench_oracle import NASBenchOracle
+except ImportError:
+    pass
+
+try:
+    from .tf_bind_8_oracle import TFBind8Oracle
+except ImportError:
+    pass
+
+try:
+    from .tf_bind_10_oracle import TFBind10Oracle
+except ImportError:
+    pass
+
+try:
+    from .toy_discrete_oracle import ToyDiscreteOracle
+except ImportError:
+    pass
