50cd4ad175c6488bd458eb0da2f49519f6e091f1
Brandon Trabucco <brandon@btrabucco.com>
2024-01-29 13:24:11 -0500

    adding error log when import fails

--- design_bench/oracles/exact/__init__.py
+++ design_bench/oracles/exact/__init__.py
@@ -1,39 +1,44 @@
 try:
-    from .hopper_controller_oracle import HopperControllerOracle
-except ImportError:
-    pass
+    from .ant_morphology_oracle import AntMorphologyOracle
+except ImportError as e:
+    print("Skipping AntMorphologyOracle import:", e)
 
 try:
-    from .ant_morphology_oracle import AntMorphologyOracle
-except ImportError:
-    pass
+    from .cifar_nas_oracle import CIFARNASOracle
+except ImportError as e:
+    print("Skipping CIFARNASOracle import:", e)
 
 try:
     from .dkitty_morphology_oracle import DKittyMorphologyOracle
-except ImportError:
-    pass
+except ImportError as e:
+    print("Skipping DKittyMorphologyOracle import:", e)
 
 try:
-    from .toy_continuous_oracle import ToyContinuousOracle
-except ImportError:
-    pass
+    from .hopper_controller_oracle import HopperControllerOracle
+except ImportError as e:
+    print("Skipping HopperControllerOracle import:", e)
 
 try:
     from .nas_bench_oracle import NASBenchOracle
-except ImportError:
-    pass
+except ImportError as e:
+    print("Skipping NASBenchOracle import:", e)
 
 try:
     from .tf_bind_8_oracle import TFBind8Oracle
-except ImportError:
-    pass
+except ImportError as e:
+    print("Skipping TFBind8Oracle import:", e)
 
 try:
     from .tf_bind_10_oracle import TFBind10Oracle
-except ImportError:
-    pass
+except ImportError as e:
+    print("Skipping TFBind10Oracle import:", e)
+
+try:
+    from .toy_continuous_oracle import ToyContinuousOracle
+except ImportError as e:
+    print("Skipping ToyContinuousOracle import:", e)
 
 try:
     from .toy_discrete_oracle import ToyDiscreteOracle
-except ImportError:
-    pass
+except ImportError as e:
+    print("Skipping ToyDiscreteOracle import:", e)
